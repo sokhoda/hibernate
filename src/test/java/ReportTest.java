6462c028d141d84fa86eb5544d333a22d54a46b1
alexandr_khoda <oleksandr.khodakovskyi@gmail.com>
2016-09-29 05:47:44 +0300

    add Report, ReportTest 2

--- src/test/java/ReportTest.java
+++ src/test/java/ReportTest.java
@@ -15,22 +15,21 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by Oleksandr_Khodakovsk on 9/28/2016.
  */
 public class ReportTest {
-    private final LocalDate now = LocalDate.now();
+    private final static LocalDate now = LocalDate.now();
     private Report rep;
-    private static LocalDate dateSample = LocalDate.of(2016,11,20);
+    private final static LocalDate dateSample = LocalDate.of(2016, 11, 20);
 
 
     @Before
     public void setUp() throws Exception {
         FitnessDataCollector fit = new FitnessDataCollector();
         TestDataSource.init(fit);
-        System.out.println();
         rep = new Report(fit);
         rep.printAll(dateSample, fit.getWater());
     }
 
     @Test
     public void dayRestWater() throws Exception {
-        assertThat(rep.dayRestWater(dateSample, DoseCollector.WATERDOSE),is(-650));
+        assertThat(rep.dayRestWater(dateSample, DoseCollector.WATERDOSE), is(-650));
     }
 }
